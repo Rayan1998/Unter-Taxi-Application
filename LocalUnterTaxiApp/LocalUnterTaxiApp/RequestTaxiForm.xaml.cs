1b6053a42691b161626cea33077d9371d1003780
ray98 <notruth500@gmail.com>
2017-05-23 13:56:30 +0200

    some minor improvements in the RequestTaxiForm

--- LocalUnterTaxiApp/LocalUnterTaxiApp/RequestTaxiForm.xaml.cs
+++ LocalUnterTaxiApp/LocalUnterTaxiApp/RequestTaxiForm.xaml.cs
@@ -16,5 +16,10 @@ namespace LocalUnterTaxiApp
 		{
 			InitializeComponent ();
 		}
-	}
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+    }
 }
