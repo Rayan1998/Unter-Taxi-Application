448b28ae153e384633907bc903ee67ab29442cb0
ray98 <notruth500@gmail.com>
2017-06-08 04:36:08 +0200

    - finished the logout user story / use case - made the password field type non bold text

--- LocalUnterTaxiApp/LocalUnterTaxiApp/UI/Account.xaml.cs
+++ LocalUnterTaxiApp/LocalUnterTaxiApp/UI/Account.xaml.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace LocalUnterTaxiApp.UI
+namespace LocalUnterTaxiApp
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class Account : ContentPage
@@ -16,5 +16,22 @@ namespace LocalUnterTaxiApp.UI
 		{
 			InitializeComponent ();
 		}
-	}
+
+
+
+        private void Save_Btn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void logout_Btn_Clicked(object sender, EventArgs e)
+        {
+            Session.Current_Customer = null;
+            Application.Current.MainPage = new NavigationPage(new MainPage())
+            {
+                BarBackgroundColor = Color.FromRgb(255, 204, 0),//this color is yellow but not too bright 
+                BarTextColor = Color.Black,
+            };
+        }
+    }
 }
--- LocalUnterTaxiApp/LocalUnterTaxiApp/UI/MainPage.xaml.cs
+++ LocalUnterTaxiApp/LocalUnterTaxiApp/UI/MainPage.xaml.cs
@@ -7,22 +7,25 @@ using Xamarin.Forms;
 
 namespace LocalUnterTaxiApp
 {
-	public partial class MainPage : ContentPage
-	{
-		public MainPage()
-		{
-			InitializeComponent();
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
 
         }
 
         private void login_btn_Clicked(object sender, EventArgs e)
         {
+
             Navigation.PushAsync(new Login());
+
         }
 
         private void register_btn_Clicked(object sender, EventArgs e)
         {
-            //Application.Current.MainPage = new NavigationPage(new Register());
+
+
             Navigation.PushAsync(new Register());
         }
     }
